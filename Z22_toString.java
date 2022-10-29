bec21288921ad1f0fda3c6f789038be3f69d65e3
Youvraj Singh <114943098+Dumb4Codes@users.noreply.github.com>
2022-10-29 18:44:00 +0530

    Add files via upload

---
+++ Z22_toString.java
@@ -0,0 +1,16 @@
+
+public class Z22_toString {
+
+	public static void main(String[] args) {
+		
+		// toString() = special method that all objects inherit,
+		//              that returns a string that "textually represents" an object.
+		//              can be used both implicitly and explicitly
+		
+		Car car = new Car();
+		System.out.println(car);
+		System.out.println(car.toString());
+
+	}
+
+}
